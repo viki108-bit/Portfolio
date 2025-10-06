1ed873f23785d889e0e94e7e4779ae770efb0cd3
Viki Gaikwad <vikigaikwad108@gmail.com>
2025-10-06 05:38:04 +0000

    go down project  (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <HeroSection />
-        <AboutSection />
         <SkillsSection />
         <ProjectsSection />
+        <AboutSection />
         <ContactSection />
       </main>
       <Footer />
