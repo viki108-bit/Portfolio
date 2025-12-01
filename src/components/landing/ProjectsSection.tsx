317512799a7709b99318b11bde3d5710990356be
Viki Gaikwad <vikigaikwad108@gmail.com>
2025-12-01 17:06:58 +0000

    delete the 1 project blickit

--- src/components/landing/ProjectsSection.tsx
+++ src/components/landing/ProjectsSection.tsx
@@ -7,15 +7,6 @@ import { ArrowUpRight, Github } from 'lucide-react';
 
 const projects = [
     {
-        title: "Blinkit Sales Dashboard",
-        description: "A Power BI dashboard analyzing Blinkit’s sales data to provide insights into total sales, item categories, outlet performance, and sales trends by outlet type.",
-        imageUrl: "/blinkit-dashboard.jpg",
-        imageDescription: "Blinkit Sales Dashboard Screenshot",
-        tags: ["Power BI", "Data Modelling", "DAX", "Data Visualization"],
-        liveUrl: "https://app.powerbi.com/view?r=eyJrIjoiYjIxMDhmMDctZWM3NS00NTk1LTk4YjMtMmQ3NWFiMzY1NzIzIiwidCI6ImQzOGI4YmJiLTg0MDYtNDVhMC05M2JiLWI0MDZkMjAwY2YzYiJ9",
-        githubUrl: "https://github.com/viki108-bit/Blinkit-Sales-Dashboard"
-    },
-    {
         title: "HR Dashboard",
         description: "An Excel dashboard to analyze employee data, highlighting demographics, performance, and salary distributions using Pivot Tables, Charts, and Slicers.",
         imageUrl: "/hr-dashboard.jpg",
